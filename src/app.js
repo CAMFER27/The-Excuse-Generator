7614158b1f96e78d15bca4149403ef0b876ad79a
CAMFER27 <cinematografico27@gmail.com>
2021-12-18 11:11:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const generateExcuse = () => {
+  let excuseGenerator = "";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  excuseGenerator += who[Math.floor(Math.random() * who.length)];
+  excuseGenerator += action[Math.floor(Math.random() * action.length)];
+  excuseGenerator += what[Math.floor(Math.random() * what.length)];
+  excuseGenerator += when[Math.floor(Math.random() * when.length)];
+
+  return excuseGenerator;
 };
+
+document.getElementById("excuse").innerHTML =
+  "OMG! you will not believe me but:" + generateExcuse();
